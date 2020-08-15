b3f9aaced7cb1584c034edfc1f4eeed6b4fdbb9c
田倩倩 <tianqianqian@tianqiaiandeMBP.lan>
2020-08-15 22:21:17 +0800

    添加app.vue文件

---
+++ views/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        这是一个demo页面
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted () {
+
+    }
+}
+</script>
