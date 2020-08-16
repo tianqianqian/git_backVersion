4788df44dbe6b93f84f39dcf1d7479908f09f99e
田倩倩 <tianqianqian@tianqiaiandeMBP.lan>
2020-08-16 09:30:34 +0800

    添加formatTime函数

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 export const add = (x, y) => {
     return x + y;
 }
+export const formatTime = (duration, type) => {
+    const endTime = new Date().valueOf()
+    const startTime = currTime - duration * 24 * 60 * 60 * 1000
+    return {
+        startTime,
+        endTime
+    }
+}
