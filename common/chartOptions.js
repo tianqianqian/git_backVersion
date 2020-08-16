1a4a755faf11f383a50c75361d17c2c21031c65e
田倩倩 <tianqianqian@tianqiaiandeMBP.lan>
2020-08-16 09:32:22 +0800

    添加样式

--- common/chartOptions.js
+++ common/chartOptions.js
@@ -2,3 +2,11 @@ export const baseOption = {
     backgroudColor: '#fff',
     color: 'red'
 }
+
+export const baseColor = {
+    backgroudColor: [
+        'red',
+        'green',
+        'blue'
+    ]
+}
