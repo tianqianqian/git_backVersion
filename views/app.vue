96905c1082e6dae211d8069efd20951a78c8bdc7
田倩倩 <tianqianqian@tianqiaiandeMBP.lan>
2020-08-16 09:25:32 +0800

    添加点击事件

--- views/app.vue
+++ views/app.vue
@@ -1,17 +1,22 @@
 <template>
     <div>
         这是一个demo页面
+        <div>
+            这是我验证问题是不是这个原因
+        </div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-
+            msg: '33'
         }
     },
     methods: {
-
+        onClickHandle () {
+            console.log('我被点击了一下')
+        }
     },
     mounted () {
 
